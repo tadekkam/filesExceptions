55e01068ba93579082cfeb44a7634e7b4705e636
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-31 01:01:39 +0100

    MOD: Create File, task0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String filePath = "d:\\data.txt";
+        String text = "Kurs" + "\n" + "Java" + "\n" + "Lekcja 6" + "\n" + "Pliki" + "\n" + "Wyjątki"
+                + "\n" + "Pliki" + "\n" + "Koniec pliku";
+        Files.writeString(Path.of(filePath), text);
     }
-
 }
