6742c2781091af3c151018161330fb657ef8e25d
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-02-03 21:59:31 +0100

    MOD: task8_OK

---
+++ src/test/java/org/example/PeselValidatorTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeselValidatorTest {
+
+    @Test
+    public void testValidatePesel() {
+        Assertions.assertDoesNotThrow(() -> PeselValidator.peselValidate("12345678911"));
+    }
+
+    @Test
+    public void testValidatePeselIllegalLength() {
+        PeselValidator.IllegalLengthException exception = assertThrows(PeselValidator.IllegalLengthException.class,
+                () -> PeselValidator.peselValidate("123"));
+        assertEquals("Numer PESEL powinien mieć dokładnie 11 znaków.", exception.getMessage());
+    }
+}
