ddb1839a60b63e2943df4cf5412009f6d045f7cb
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-02-03 15:35:30 +0100

    MOD: task4

---
+++ src/main/java/org/example/FileExample.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
+public class FileExample {
+
+    public static boolean isTextFindFile (String filePath, String textFind) throws Exception {
+
+        String text = Files.readString(Path.of(filePath));
+        text = text.toUpperCase();
+        textFind = textFind.toUpperCase();
+
+        if (text.contains(textFind)) {
+            return true;
+        }
+        return false;
+
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        String filePath = "D:\\data.txt";
+        String textFind = "JAVA";
+
+        try {
+            boolean isTextFind = FileExample.isTextFindFile(filePath, textFind);
+            System.out.println(isTextFind);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
