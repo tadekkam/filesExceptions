0ab0d6d3e032b43cb521be7fa21d95b7c5b9c087
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-02-04 08:11:32 +0100

    MOD: task9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        Validator validator = new Validator();
+        int[] values = {9, 0, 15, 7, 18, -4};
+
+        validator.validate(values);
+
     }
 
 }
